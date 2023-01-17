bc3f2cacc58ddffc0d68c2802f015100515658d5
Rafael Bañón <rafael.banon-martin@capgemini.com>
2023-01-17 16:09:35 +0100

    Security

--- src/api/weather-api.js
+++ src/api/weather-api.js
@@ -2,9 +2,6 @@ import axios from 'axios';
 
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&lang={lang}
 
-// export const API_KEY = '5ad4cc52b04e6c53ee69b3229a9f1cbd';
-// export const API_LANG = 'es';
-
 export const weatherApi = axios.create({
   baseURL: 'https://api.openweathermap.org/data/2.5/weather',
 });
